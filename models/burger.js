ceb8d828e45b4648d2b888721fc0a1afe51c4fdc
Jesse Thomas <jayayetea@gmail.com>
2017-04-30 22:05:26 -0500

    converted to sequelize instead of using orm

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,48 @@
-var orm = require("../config/orm.js");
+// var orm = require("../config/orm.js");
 
-var burger = {
+// var burger = {
 
-	selectAll: function(cb) {
-		orm.selectAll("burgers", function(res) {
-			console.log("Model");
-			cb(res);
-		});
-	},
-	insertOne: function(cols, vals, cb) {
-		orm.insertOne("burgers", cols, vals, function(res){
-			console.log("Model");
-			cb(res);
-		});
-	},
-	updateOne: function(colVals, condition, cb) {
-		orm.updateOne("burgers", colVals, condition, function(res) {
-			console.log("Model");
-			cb(res);
-		});
-	}
+// 	selectAll: function(cb) {
+// 		orm.selectAll("burgers", function(res) {
+// 			console.log("Model");
+// 			cb(res);
+// 		});
+// 	},
+// 	insertOne: function(cols, vals, cb) {
+// 		orm.insertOne("burgers", cols, vals, function(res){
+// 			console.log("Model");
+// 			cb(res);
+// 		});
+// 	},
+// 	updateOne: function(colVals, condition, cb) {
+// 		orm.updateOne("burgers", colVals, condition, function(res) {
+// 			console.log("Model");
+// 			cb(res);
+// 		});
+// 	}
 
+// };
+
+// module.exports = burger;
+
+// OLD ^^^^^^^^
+
+module.exports = function(sequelize, dataTypes){
+	var Burger = sequelize.define("Burger", {
+		burger_name : {
+			type: DataTypes.STRING,
+			allowNull: false,
+		    validate: {
+		    	len: [255]
+		    }
+		},
+		devoured : {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			validate: {
+				len: [1]
+			}
+		}
+	});
+	return Burger;
 };
-
-module.exports = burger;
